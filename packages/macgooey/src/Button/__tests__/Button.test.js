5bfbc12492927c83bfcb37c7a64f8f805d8dcd03
Thomas Lingham <tom.lingham@iag.com.au>
2018-01-10 10:39:10 +1100

    Prettier

--- packages/macgooey/src/Button/__tests__/Button.test.js
+++ packages/macgooey/src/Button/__tests__/Button.test.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Button } from "..";
 import "jest-styled-components";
 
-
 import renderer from "react-test-renderer";
 import { themeBase } from "../../theme";
 
@@ -16,21 +15,27 @@ describe("<Button />", () => {
   });
   test("it should render the primary button", () => {
     const component = renderer.create(
-      <Button theme={themeBase} color="primary">Test Button</Button>
+      <Button theme={themeBase} color="primary">
+        Test Button
+      </Button>
     );
 
     expect(component.toJSON()).toMatchSnapshot();
   });
   test("it should render the secondary button", () => {
     const component = renderer.create(
-      <Button theme={themeBase} color="secondary">Test Button</Button>
+      <Button theme={themeBase} color="secondary">
+        Test Button
+      </Button>
     );
 
     expect(component.toJSON()).toMatchSnapshot();
   });
   test("it should render the tertiary button", () => {
     const component = renderer.create(
-      <Button theme={themeBase} color="tertiary">Test Button</Button>
+      <Button theme={themeBase} color="tertiary">
+        Test Button
+      </Button>
     );
 
     expect(component.toJSON()).toMatchSnapshot();
