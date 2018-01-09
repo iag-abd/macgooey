5964e55efed3130c69f4a58785b738686a165334
Thomas Lingham <tom.lingham@iag.com.au>
2018-01-10 10:36:08 +1100

    Adding test coverage and coveralls support

--- packages/macgooey/src/Button/__tests__/Button.test.js
+++ packages/macgooey/src/Button/__tests__/Button.test.js
@@ -1,18 +1,38 @@
 import React from "react";
 import { Button } from "..";
-import { withBaseTheme } from "../../theme/utils";
 import "jest-styled-components";
 
+
 import renderer from "react-test-renderer";
+import { themeBase } from "../../theme";
 
 describe("<Button />", () => {
   test("it should render", () => {
     const component = renderer.create(
-      withBaseTheme(<Button>Test Button</Button>)
+      <Button theme={themeBase}>Test Button</Button>
     );
 
-    let tree = component.toJSON();
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+  test("it should render the primary button", () => {
+    const component = renderer.create(
+      <Button theme={themeBase} color="primary">Test Button</Button>
+    );
 
-    expect(tree).toMatchSnapshot();
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+  test("it should render the secondary button", () => {
+    const component = renderer.create(
+      <Button theme={themeBase} color="secondary">Test Button</Button>
+    );
+
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+  test("it should render the tertiary button", () => {
+    const component = renderer.create(
+      <Button theme={themeBase} color="tertiary">Test Button</Button>
+    );
+
+    expect(component.toJSON()).toMatchSnapshot();
   });
 });
